dd55a03f8a2162607f332f2c8fee4c5eedbe89e7
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-05-21 12:00:40 -0600

    Added code profiler class

---
+++ bin/RokkoMVC/Profiler.php
@@ -0,0 +1,68 @@
+<?php
+namespace Rokko;
+
+class Profiler {
+	private $microSum;
+	private $tmpMicroSum;
+	private $count;
+
+	const SECONDS = 0;
+	const MINUTES = 1;
+	const HOURS = 2;
+	const TIME = 3;
+
+	public function __construct() {
+		$this->microSum = 0;
+		$this->tmpMicroSum = null;
+		$this->count = 0;
+	}
+
+	public function clear() {
+		$this->microSum = 0;
+		$this->tmpMicroSum = null;
+		$this->count = 0;
+	}
+
+	public function start() {
+		$this->tmpMicroSum = microtime(true);
+	}
+
+	public function end() {
+		if ($this->tmpMicroSum != null) {
+			$this->microSum += microtime(true) - $this->tmpMicroSum;
+			$this->tmpMicroSum = null;
+			$this->count++;
+		}
+	}
+
+	public function getAverage($format = self::SECONDS) {
+		switch ($format) {
+			case self::SECONDS:
+				return number_format($this->microSum / $this->count, 4);
+			case self::MINUTES:
+				return number_format($this->microSum / $this->count / 60, 4);
+			case self::HOURS:
+				return number_format($this->microSum / $this->count / 60 / 60, 4);
+			default:
+				return number_format($this->microSum / $this->count, 4);
+		}
+	}
+
+	public function getTotal($format = self::SECONDS) {
+		switch ($format) {
+			case self::SECONDS:
+				return number_format($this->microSum, 4);
+			case self::MINUTES:
+				return number_format($this->microSum / 60, 4);
+			case self::MINUTES:
+				return number_format($this->microSum / 60 / 60, 4);
+			default:
+				return number_format($this->microSum, 4);
+		}
+	}
+
+	public function getCount($format) {
+		return $this->count;
+	}
+}
+
