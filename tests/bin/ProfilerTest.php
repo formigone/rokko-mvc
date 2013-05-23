69fbd2b6c46d89c1eaca92c01a4833bb4a2b4965
Rodrigo Silveira <webmaster@rodrigo-silveira.com>
2013-05-22 21:55:29 -0600

    Added a few test cases to validate Rokko\Profiler

--- tests/bin/ProfilerTest.php
+++ tests/bin/ProfilerTest.php
@@ -1,8 +1,105 @@
 <?php
 require_once("./PHPUnit/Autoload.php");
+require_once("./bootstrap.php");
 
 class RokkoMVC_ProfilerTest extends PHPUnit_Framework_TestCase {
-	public function testSetup_shouldPassTrivial() {
-		$this->assertTrue(true);
+	private $prof;
+
+	public function setup() {
+		$this->prof = new MockProfiler();
+	}
+
+	public function testCount_noTimers_shouldBeZero() {
+		$count = $this->prof->getCount();
+		$this->assertEquals(0, $count);
+	}
+
+	public function testTotal_noTimers_shouldBeZero() {
+		$total = $this->prof->getTotal(\Rokko\Profiler::SECONDS);
+		$this->assertEquals(0, $total);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::MINUTES);
+		$this->assertEquals(0, $total);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::HOURS);
+		$this->assertEquals(0, $total);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("00:00:00", $total);
+	}
+
+	public function testTotal_openTimer_shouldBeZero() {
+		$this->prof->start();
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::SECONDS);
+		$this->assertEquals(0, $total);
+	
+		$total = $this->prof->getTotal(\Rokko\Profiler::MINUTES);
+		$this->assertEquals(0, $total);
+	
+		$total = $this->prof->getTotal(\Rokko\Profiler::HOURS);
+		$this->assertEquals(0, $total);
+	
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("00:00:00", $total);
+	}
+
+	public function testTotal_closedTimerOneSec_shouldBeOneSec() {
+		$PHP_SLEEP_DISCLAIMER = "Tough to test because this relies on the accuracy of PHP's sleep()";
+		$this->prof->start();
+		sleep(1);
+		$this->prof->end();
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::SECONDS);
+		$this->assertTrue($total >= 1.0 - 0.5 && $total < 1.5, $PHP_SLEEP_DISCLAIMER);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::MINUTES);
+		$this->assertTrue($total >= 1/60 - 0.5 && $total < 0.1, $PHP_SLEEP_DISCLAIMER);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::HOURS);
+		$this->assertTrue($total >= 1/60/60 - 0.5 && $total < 0.1, $PHP_SLEEP_DISCLAIMER);
+	}
+
+	public function testTotal_injectedSecond_shouldBeOneSec() {
+		$sec = 1.0000;
+		$min = number_format($sec / 60, 4);
+		$hour = number_format($sec / 60 / 60, 4);
+		$this->prof->setSeconds($sec);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::SECONDS);
+		$this->assertEquals($sec, $total);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::MINUTES);
+		$this->assertEquals($min, $total);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::HOURS);
+		$this->assertEquals($hour, $total);
+
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("00:00:01", $total);
+	}
+
+	public function testTotal_complexInjectedTime_shouldFormatAsTime() {
+		$this->prof->setSeconds(125.0000);
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("00:02:05", $total);
+
+		$this->prof->setSeconds(345.000);
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("00:05:45", $total);
+
+		$this->prof->setSeconds(76543.000);
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("21:15:43", $total);
+
+		$this->prof->setSeconds(59022.000);
+		$total = $this->prof->getTotal(\Rokko\Profiler::TIME);
+		$this->assertEquals("16:23:42", $total);
+	}
+}
+
+class MockProfiler extends \Rokko\Profiler {
+	public function setSeconds($seconds) {
+		$this->microSum = $seconds;
 	}
 }
