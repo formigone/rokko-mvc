8a789d612daa5b6ec557b3606b68b990e57b3278
formigone <ax198_11@yahoo.com>
2013-05-27 15:26:20 -0600

    Updated tests bootstrap due to changes in autoloader class. Autoloader class must be instantiated in order for it to work, since PHP hooks take place in class constructor

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,7 @@ define("BIN_PATH", realpath(__DIR__)."/../bin/RokkoMVC");
 define("APP_PATH", realpath(__DIR__)."/../app");
 
 require_once(BIN_PATH."/Autoloader.php");
+
+$appConfig = json_decode(file_get_contents(APP_PATH."/config/config.json"), true);
+$rokkoConfig = json_decode(file_get_contents(BIN_PATH."/config/config.json"), true);
+$autoloader = new \Rokko\Autoloader($appConfig, $rokkoConfig);
